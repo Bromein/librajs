d353704f65027c20d527f9de75b0a2c401493e26
Bromein <ajbroman@gmail.com>
2024-11-04 20:16:27 -0500

    some styles added

---
+++ app.js
@@ -0,0 +1,13 @@
+const myLibrary = [];
+
+
+function Book(title, author, pages, genre) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.genre = genre;
+}
+
+function addBookToLibrary(book) {
+    
+}
